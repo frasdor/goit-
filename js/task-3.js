e8efd61050d4b21142587d1029eef49bca1f2686
frasdor <frasunkiewicz.dorota@gmail.com>
2024-08-20 15:31:08 +0200

    Updated task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,28 @@
-function changeUsername(newName){
+const profile = {
+	username: "Jacob",
+  playTime: 300,
+
+changeUsername(newName){
+   this.username = newName;
+},
+updatePlayTime(hours){
+    this.playTime += hours;
+},
+getInfo(){
+    let amount = 0;
+    amount = this.playTime;
+
+    return`${this.username} has ${amount} active hours!`
+}
 
 };
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
 
 
@@ -13,11 +34,5 @@ function changeUsername(newName){
 
 
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
